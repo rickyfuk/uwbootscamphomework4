4eb633597a8cf5af29362ca80d975f3291c83dd4
digilau <rickyfukfuk@gmail.com>
2020-06-29 13:33:14 -0700

    initial commit

---
+++ assets/javascript/main.js
@@ -0,0 +1,180 @@
+var timer = document.getElementById("timer");
+// set a var for timeInterval as novalue to stop the function
+var timeInterval;
+// the initial second count (should be 75)
+var secondLeft = 5;
+// locate the main body
+var mainBody = document.getElementById("initialBody");
+// locate the "Start Quiz Button"
+var startQ = document.querySelector("#startQuiz");
+// locate the question body
+var questionBody =  document.getElementById("questionBody");
+// locate the question number (h1)
+var questionNum = document.getElementById("questionNum");
+// locate the question Title (p)
+var questionTitle = document.getElementById("questionTitle");
+// locate the choice list (ul)
+var choiceList = document.getElementById("choiceList");
+// i for question counter (default as 0)
+var i = 0; 
+// locate the user choice for the questions
+var userChoice = document.getElementsByClassName("userChoice");
+
+
+
+
+
+// function 1 - counts second left
+function countSecondLeft (){
+    secondLeft--;
+    // add an function to find out the question true/false
+    // if false, then secondLeft = secondLeft - 10;
+    timer.textContent = "Time : " + secondLeft;
+
+    if (secondLeft === 0){
+        clearInterval(timeInterval);
+        // add a function to display the result
+        // displayResult ();
+    }
+}
+
+// function 2 - execute time interval 
+function countDownExecute(){
+    timeInterval = setInterval(countSecondLeft,1000);
+}
+
+// function 3 - display the question and get the result
+function questionLoop(){
+
+        addQuestion(i);
+    // if (i<5){
+    //     // add the question number on the question heading
+    //     questionNum.textContent = "Question " + (i+1);
+    //     // add the question title content
+    //     questionTitle.textContent = questions[i].title;
+    //     // add the answer choice
+    //     for (j=0; j<questions[i].choices.length; j++){
+    //         var li = document.createElement("li");
+    //         var choiceBtn = document.createElement("button");
+    //         // create a list item with a button inside for displaying the selection
+    //         choiceBtn.textContent = questions[i].choices[j];    
+    //         choiceBtn.setAttribute("class","userChoice mb-3 btn btn-outline-primary");            
+    //         choiceList.appendChild(li);
+    //         li.appendChild(choiceBtn);
+    //     }
+        showQuestionBody();
+        checkAnswer();
+        // // add eventListener for getting the user response
+        // // locate the user choice for the questions
+        // var userChoice = document.getElementsByClassName("userChoice");
+        // userChoice.addEventListener()("click",function(event){
+        //     event.preventDefault();
+        //     var userSelect = event.target;
+        //     // check if the selection is a button
+        //     if (userSelect.matches("button") === true){
+        //         // check if the selection match with the correct answer
+        //         if (userSelect.textContent.matches(questions[i].answer) === true){
+        //             var feedback = document.createElement("p");
+        //             feedback.textContent = "Correct"
+        //             li.appendChild(feedback);
+        //         }
+        //         else {
+        //             feedback.textContent = "Wrong! The coorect answer is " + questions[i].answer;
+        //             li.appendChild(feedback); 
+        //         }
+        //     }      
+        // }
+        // )
+        i++;
+          
+    
+
+}
+
+// function 3.1 - add the question layout
+function addQuestion(i){
+    if (i<5){
+        // add the question number on the question heading
+        questionNum.textContent = "Question " + (i+1);
+        // add the question title content
+        questionTitle.textContent = questions[i].title;
+        // add the answer choice
+        for (j=0; j<questions[i].choices.length; j++){
+            var li = document.createElement("li");
+            var choiceBtn = document.createElement("button");
+            // create a list item with a button inside for displaying the selection
+            choiceBtn.textContent = questions[i].choices[j];    
+            choiceBtn.setAttribute("class","userChoice mb-3 btn btn-outline-primary");            
+            choiceList.appendChild(li);
+            li.appendChild(choiceBtn);
+        }
+    }   
+    console.log(userChoice);
+    // return(userChoice);
+}
+
+// function 3.2  - check the user response with the answer
+function checkAnswer(){
+    // add eventListener for getting the user response
+        // locate the user choice for the questions
+        // var userChoice = document.getElementsByClassName("userChoice");
+        for(var k = 0; k < userChoice.length; k++) {
+                userChoice[k].addEventListener()("click",function(event){
+                    var userSelect = event.target;
+                    // check if the selection is a button
+                    if (userSelect.matches("button") === true){
+                        event.preventDefault();
+                        // check if the selection match with the correct answer
+                        if (userSelect.textContent.matches(questions[i].answer) === true){
+                            var feedback = document.createElement("p");
+                            feedback.textContent = "Correct"
+                            li.appendChild(feedback);
+                        }
+                        else {
+                            feedback.textContent = "Wrong! The coorect answer is " + questions[i].answer;
+                            li.appendChild(feedback); 
+                        }
+                    }      
+                }
+                )
+        }
+            
+}
+
+
+
+// function 4 - hide the body content
+function clearMainBody(){
+    mainBody.setAttribute ("style", "display:none;");
+}
+
+// function 5 - show the body content
+function showMainBody(){
+    mainBody.setAttribute ("style", "display:block;");
+}
+
+// function 6 - hide the question content
+function clearQuestionBody(){
+    questionBody.setAttribute ("style", "display:none;");
+}
+
+// function 7 - show the question content
+function showQuestionBody(){
+    questionBody.setAttribute ("style", "display:block;");
+}
+
+
+clearQuestionBody();
+// After the start quiz button is clicked
+// exectue function countDownExecute
+startQ.addEventListener("click",function(event){
+    event.preventDefault();
+    clearMainBody();
+    countDownExecute();
+    questionLoop();
+}
+)
+
+console.log(startQ);
+console.log(questions[1].title);
+console.log(choiceList);
---
+++ assets/javascript/questionScript.js
@@ -0,0 +1,36 @@
+var questions = [
+    {
+      title: 'Commonly used data types DO NOT include:',
+      choices: ['strings', 'booleans', 'alerts', 'numbers'],
+      answer: 'alerts'
+    },
+    {
+      title: 'The condition in an if / else statement is enclosed within ____.',
+      choices: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
+      answer: 'parentheses'
+    },
+    {
+      title: 'What is the return value for "length()"',
+      choices: ['the index within the calling String object of the first occurrence of the specified value', 
+                'the length of the string', 
+                'removes the last element from an array and returns that element', 
+                'all of the above'],
+      answer: 'the length of the string'
+    },
+    {
+      title: 'What are the variable naming conventions in JavaScript?',
+      choices: ['should not use any of the JavaScript reserved keyword', 
+                'should not start with a numeral (0-9)', 
+                'case sensitive', 
+                'all of the above'],
+      answer: 'all of the above'
+    },
+    {
+      title: 'What would be the result of 2+5+”3″?',
+      choices: ['73', 
+                '10', 
+                '253', 
+                'none of the above'],
+      answer: '73'
+    }
+]
